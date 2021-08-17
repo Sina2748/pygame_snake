fb28cf9e43fe2f0b7b92766962bdb3a85c16538b
Sina Samii <sina.sam@gmail.com>
2021-08-18 01:48:58 +0430

    Fruit body

--- snake_pygame.py
+++ snake_pygame.py
@@ -1,9 +1,11 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.math import Vector2
+
+### Making classes
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,9), Vector2(4,9), Vector2(3,9)]
-        self.direction = Vector2(-1,0)
+        self.direction = Vector2(1,0)
 
     def draw_snake(self):
         for block in self.body:
@@ -13,27 +15,58 @@ class SNAKE:
             block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
             pygame.draw.rect(screen, (255,255,255), block_rect)
 
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0, 1200 - 1)
+        self.y = random.randint(0, 800 - 1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x, self.pos.y, cell_size, cell_size)
+        pygame.draw.rect(screen, (255,255,255), fruit_rect)
+
+### makeing of the objects
 snake = SNAKE()
-  
+fruit = FRUIT()
+
+### Makeing the screen
 pygame.init() 
 cell_number = 10
 cell_size = 20
-
 screen =pygame.display.set_mode((cell_number*cell_size*6, cell_number*cell_size*4))
+
+### Events
 clock = pygame.time.Clock()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-
+### Main Loop of the game
 while True:
+
+    #Quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+
+    #Show screen
     screen.fill((75, 15, 70))
+
+    #Show snake
     snake.draw_snake()
 
+    #Show fruit
+    fruit.draw_fruit()
 
+    #STUFF
     pygame.display.update()
     clock.tick(60)
 
