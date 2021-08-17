0336f86cd2d5bff94a4a175f302ea57f7e2356c1
Sina Samii <sina.sam@gmail.com>
2021-08-17 16:21:37 +0430

    1st commit

---
+++ snake_pygame.py
@@ -0,0 +1,16 @@
+import pygame, sys
+
+pygame.init()
+pygame.display.set_mode((400, 500))
+clock = pygame.time.Clock()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
+
+"""jhgygyg"""
